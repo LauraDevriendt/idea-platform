5703098730f6165cc2a5bc86860ebaf68e873400
Nicholas Ocket <nicholas.ocket@lightspeedhq.com>
2020-11-11 14:06:42 +0100

    first commit: setting gitignore

---
+++ src/test/java/com/coaching/salesplatform/SalesPlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package com.coaching.salesplatform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SalesPlatformApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
