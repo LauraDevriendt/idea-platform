dc03c4dfe3aa25b67e0b85a0dfd14f3a8a1edcec
LauraDevriendt <laura_devriendt5@hotmail.com>
2020-11-11 16:03:54 +0100

    relational setup between entities

---
+++ src/main/java/com/coaching/salesplatform/customer/Address.java
@@ -0,0 +1,33 @@
+package com.coaching.salesplatform.customer;
+
+import lombok.Data;
+
+import javax.persistence.*;
+import java.util.Set;
+
+@Entity
+@Table(name = "addresses")
+@Data
+public class Address {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "STREETNAME")
+    private String streetName;
+
+    @Column(name = "CITY")
+    private String city;
+
+    @Column(name = "NUMBER")
+    private Integer number;
+
+    @Column(name = "ZIPCODE")
+    private String zipcode;
+
+    @ManyToMany(mappedBy = "addresses")
+    private Set<Customer> customers;
+
+
+}
